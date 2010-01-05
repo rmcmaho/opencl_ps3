cb130c96b16e05142cb4dc8e91d910ece50fe9be
Robert McMahon <RMcmahon@qamc-103-tw-int-5043.corp.blizzard.net>
2010-01-04 22:46:14 -0800

    Fixed indentation in kernel_parser.lua

--- src/lua/kernel_parser.lua
+++ src/lua/kernel_parser.lua
@@ -11,7 +11,7 @@ function loadFile(filename)
    end
    
    FILE = file
-
+   
 end
 
 
@@ -19,40 +19,40 @@ end
 function getNextKernel()
    
    local file = FILE
-
+   
    while true do
       local line = file:read("*line")
-
+      
       if line == nil then 
       	 -- End of file 
      	 return nil, nil, nil
       end
-
+      
       if line:find("__kernel") ~= nil then
-
-      	  --- Check for new lines (will break if extra parentheses are used)
-      	  local func_end = line
-      	  local s = newStack()      
-      	  while func_end:find(")") == nil
-      	  do
-		addString(s, func_end)
-		func_end = file:read("*line")
-          end
-      	  addString(s, func_end)
-      	  s = table.concat(s)
-      	  line = s
-      	  ---
-	  local funcName = getKernelName(line)
+	 
+	 --- Check for new lines (will break if extra parentheses are used)
+	 local func_end = line
+	 local s = newStack()      
+	 while func_end:find(")") == nil
+	 do
+	    addString(s, func_end)
+	    func_end = file:read("*line")
+	 end
+	 addString(s, func_end)
+	 s = table.concat(s)
+	 line = s
+	 ---
+	 local funcName = getKernelName(line)
 	 
 	 -- Why does this give "unfinished capture"?
 	 --local beginParam = line:find("(")
 	 local begin, finish = line:find("%s+%w+%s-%(")	
 	 local beginParam = finish+1
 	 local endParam = line:find(",")
-
+	 
 	 local param_table = {}
 	 local paramCount = 1	 
-
+	 
 	 while endParam ~= nil
 	 do
 	    table.insert(param_table, trim(string.sub(line, beginParam, endParam-1)))
@@ -60,31 +60,31 @@ function getNextKernel()
 	    beginParam = endParam+1
 	    endParam = line:find(",",beginParam)
 	 end --End little while
-
+	 
 	 endParam = line:find(")", beginParam)
 	 table.insert(param_table, trim(string.sub(line, beginParam, endParam-1)))
-
+	 
 	 return funcName, paramCount, param_table
-
-     end -- End big if
-
+	 
+      end -- End big if
+      
    end -- End big while
-
+   
    return nil, nil, nil 
-	    
+   
 end
 
 
 --Get kernel name from line and trim white space
 function getKernelName(s)
-
+   
    local begin, finish = s:find("%s+%w+%s-%(")
    
    local funcName = s:sub(begin, finish-1)
    
    funcName = trim(funcName)
    return funcName
-
+   
 end
 
 
@@ -96,13 +96,13 @@ end
 function newStack ()
    return {""}   -- starts with an empty string
 end
-    
+
 function addString (stack, s)
    table.insert(stack, s)    -- push 's' into the the stack
    -- What does this loop do?
    for i=table.getn(stack)-1, 1, -1 do       
       if string.len(stack[i]) > string.len(stack[i+1]) then
-          break
+	 break
       end
       stack[i] = stack[i] .. table.remove(stack)
    end
